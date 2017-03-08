b6eeccd7ca19d53b50cc01160ad234514903f7bf
markcanwell <markcanwell@gmail.com>
2017-03-08 08:51:06 +0000

    Initial Commit

---
+++ GSGPrototypeCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GSGPrototypeCell
+//
+//  Created by Mark Canwell on 08/03/2017.
+//  Copyright © 2017 GSG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GSGPrototypeCellTests/GSGPrototypeCellTests.m
@@ -0,0 +1,39 @@
+//
+//  GSGPrototypeCellTests.m
+//  GSGPrototypeCellTests
+//
+//  Created by Mark Canwell on 08/03/2017.
+//  Copyright © 2017 GSG. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GSGPrototypeCellTests : XCTestCase
+
+@end
+
+@implementation GSGPrototypeCellTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
